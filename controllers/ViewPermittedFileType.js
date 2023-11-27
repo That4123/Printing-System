32cf3cc77e84daca76dff4ef286deae913b58608
Dung Vo <90669649+vandungvo@users.noreply.github.com>
2023-11-28 00:59:03 +0700

    Update ViewPermittedFileType.js

--- controllers/ViewPermittedFileType.js
+++ controllers/ViewPermittedFileType.js
@@ -1,12 +1,60 @@
-const express = require('express');
-const ViewPermittedFileType = require('../controllers/ViewPermittedFileType');
+const permittedFileTypeModel = require('../model/DAO/permitted_file_type_DAO');
+const spsoModel = require('../model/DAO/SPSO_DAO');
 const path = require("path");
-const ViewPermittedFileType_router = express.Router();
 
-ViewPermittedFileType_router.post('/', ViewPermittedFileType.getPermittedFileTypeList);
+const getPermittedFileTypeList = (req, res) => {
+    permittedFileTypeModel.getPermittedFileTypes((err, permittedFileTypes) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.json(permittedFileTypes);
+        }
+    })
+};
 
-ViewPermittedFileType_router.post('/add', ViewPermittedFileType.addNewPermittedFileType);
-ViewPermittedFileType_router.post('/edit', ViewPermittedFileType.editPermittedFileType);
-ViewPermittedFileType_router.post('/remove', ViewPermittedFileType.removePermittedFileType);
+function addNewPermittedFileType(req, res) {
+    let permittedFileType = {
+        file_type: req.body.file_type,
+        max_file_size: req.body.max_file_size
+    };
+    spsoModel.addNewPermittedFileType(permittedFileType, function (err, result) {
+        if (err) {
+            res.status(err.code).json({ message: err.message });
+        }
+        else {
+            res.json({ message: "Thêm loại file được phép in thành công!" })
+        }
+    })
+}
 
-module.exports = ViewPermittedFileType_router;
+function editPermittedFileType(req, res) {
+    let permittedFileType = {
+        file_type: req.body.file_type,
+        max_file_size: req.body.max_file_size
+    };
+    spsoModel.editPermittedFileType(permittedFileType, function (err, result) {
+        if (err) {
+            res.status(err.code).json({ message: err.message });
+        } else {
+            res.json({ message: "Cập nhật thông tin loại file được phép in thành công!" })
+        }
+    })
+}
+
+function removePermittedFileType(req, res) {
+    spsoModel.removePermittedFileType(req.body.permitted_id, function (err, result) {
+        if (err) {
+            res.status(err.code).json({ message: err.message });
+        } else {
+            res.json({ message: "Xóa loại file được phép in thành công!" })
+        }
+    })
+}
+
+module.exports = {
+    getPermittedFileTypeList,
+    addNewPermittedFileType,
+    editPermittedFileType,
+    removePermittedFileType
+};
