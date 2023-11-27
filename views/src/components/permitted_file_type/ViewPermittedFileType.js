94b2d9e1b6f6bf79ccb4dcf7183c0f48a5bb83a4
Dung Vo <dung.vovandung@hcmut.edu.vn>
2023-11-28 02:16:35 +0700

    Update view permitted file type

--- views/src/components/permitted_file_type/ViewPermittedFileType.js
+++ views/src/components/permitted_file_type/ViewPermittedFileType.js
@@ -11,17 +11,39 @@ import Header from '../shared/header';
 import Cookies from "universal-cookie";
 import '../../styles/themify-icons/themify-icons.css';
 import { useParams } from 'react-router-dom';
+import "./ViewPermittedFileType.css";
 
 const cookies = new Cookies();
 
 function ViewPermittedFileType() {
     const [permittedFileTypes, setPermittedFileTypes] = useState([]);
+    const [showPopup, setShowPopup] = useState(false);
+    const [newFileType, setNewFileType] = useState('');
+    const [newMaxFileSize, setNewMaxFileSize] = useState('');
 
     useEffect(() => {
         axios.post('/api/viewPermittedFileType')
         .then(response => setPermittedFileTypes(response.data))
         .catch(error => console.error('Error fetching permitted file types:', error));
     }, []);
+
+    const handleAddButtonClick = () => {
+        setShowPopup(true);
+    }
+
+    const handlePopupClose = () => {
+        setShowPopup(false);
+        setNewFileType('');
+        setNewMaxFileSize('');
+    }
+    const handleAddFileSubmit = () => {
+        // Perform the submission logic here
+        // ...
+    
+        // After successful submission, close the pop-up and refresh the list
+        handlePopupClose();
+        // Fetch the updated list of permitted file types here
+      };
 
     return (
         <>
@@ -48,7 +70,42 @@ function ViewPermittedFileType() {
                     ))}
                 </tbody>
             </table>
-            <button clássName='btn btn-primary'>Thêm</button>
+            <button className='btn btn-primary' onClick={handleAddButtonClick}>Thêm</button>
+
+            {showPopup && (
+                <div className="popup-overlay">
+                <div className="popup">
+                    <h2 className="mt-1">Thêm loại file</h2>
+                    <form onSubmit={handleAddFileSubmit}>
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td>Loại file:</td>
+                            <td>
+                            <input
+                                type="text"
+                                value={newFileType}
+                                onChange={e => setNewFileType(e.target.value)}
+                            />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Kích thước tối đa:</td>
+                            <td>
+                            <input
+                                type="text"
+                                value={newMaxFileSize}
+                                onChange={e => setNewMaxFileSize(e.target.value)}
+                            />
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <button className="btn btn-dark mt-2" type="submit">Xác nhận</button>
+                    </form>
+                </div>
+                </div>
+            )}
         </>
     );
 }
