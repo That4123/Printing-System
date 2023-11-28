11f4a40ad12fad002e03eb682f8239cd96586479
thái giản đình <giandinhthai@gmail.com>
2023-11-28 12:19:07 +0700

    add printing_log

--- hcmut_spss.sql
+++ hcmut_spss.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 16, 2023 lúc 04:08 PM
--- Phiên bản máy phục vụ: 10.4.27-MariaDB
--- Phiên bản PHP: 8.2.0
+-- Host: 127.0.0.1
+-- Generation Time: Nov 28, 2023 at 06:18 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,45 +18,15 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `hcmut_spss`
+-- Database: `hcmut_spss`
 --
-CREATE DATABASE IF NOT EXISTS `hcmut_spss` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `hcmut_spss`;
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `printer`
+-- Table structure for table `user`
 --
-
-CREATE TABLE `printer` (
-  `printer_id` int(11) NOT NULL,
-  `brand` varchar(255) NOT NULL,
-  `model` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `campusName` varchar(255) NOT NULL,
-  `roomNumber` varchar(63) NOT NULL,
-  `buildingName` varchar(63) NOT NULL,
-  `printer_status` varchar(127) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
---
--- Đang đổ dữ liệu cho bảng `printer`
---
-
-INSERT INTO `printer` (`printer_id`, `brand`, `model`, `description`, `campusName`, `roomNumber`, `buildingName`, `printer_status`) VALUES
-(1, 'Canon', 'LBP 6030', 'Mới, không lem mực', '2', '101', 'H1', 'Đang hoạt động'),
-(2, 'Xerox', 'XR1737', 'Mới, không lem mực', '1', '203', 'C5', 'Đang hoạt động'),
-(3, 'HP', '2Z609A', 'Mới, không lem mực', '2', '303', 'H3', 'Đang hoạt động'),
-(4, 'Canon', 'LBP 4039', 'Mới, không lem mực', '1', '314', 'B1', 'Đang hoạt động'),
-(5, 'Epson', 'ESP030', 'Mới, không lem mực', '2', '403', 'H3', 'Đang hoạt động'),
-(6, 'Canon', 'G1737', 'Mới, không lem mực', '1', '101', 'B7', 'Đang hoạt động'),
-(7, 'HP', '2T394A', 'Mới, không lem mực', '2', '303', 'H6', 'Đang hoạt động'),
-(8, 'Canon', 'LBP 5023', 'Mới, không lem mực', '1', '504', 'B1', 'Đang hoạt động');
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `user`
+-- Creation: Nov 27, 2023 at 03:12 PM
 --
 
 CREATE TABLE `user` (
@@ -69,115 +39,28 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `user`
+-- RELATIONSHIPS FOR TABLE `user`:
+--
+
+--
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`user_id`, `user_name`, `email`, `password`, `role`, `state`) VALUES
 (2000000, 'Admin 1', 'abc@def.com', '$2b$10$gmZJOS1l9LGpT3HyDPMWkOx.E23.QneEZ0ISy9FDosVE9g8zBz2mm', 'Quản trị viên', 'Đang hoạt động'),
 (2000001, 'Student 1', 'abd@def.com', '$2b$10$vPfiwJdRbJsMV04pbav0TOry.VJe9Of6Gdo08d0mVh9VMpjpJUJHy', 'Sinh viên', 'Đang hoạt động');
 
--- --------------------------------------------------------
 --
--- Cấu trúc bảng cho bảng `print_request`
---
-
-CREATE TABLE `print_request` (
-  `request_id` int(11) NOT NULL,
-  `file_name` varchar(255) NOT NULL,
-  `file_path` varchar(255) NOT NULL,
-  `chosen_printer` int(11) NOT NULL,
-  `paper_size` varchar(127) NOT NULL,
-  `pages_to_print` varchar(127) NOT NULL,
-  `is_double_side` tinyint(1) NOT NULL,
-  `number_of_copies` int(11) NOT NULL,
-  `print_type` varchar(127) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `permitted_file_type`
---
-
-CREATE TABLE `permitted_file_type` (
-  `permitted_id` int(11) NOT NULL,
-  `file_type` varchar(255) NOT NULL,
-  `max_file_size` int(127) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Đang đổ dữ liệu cho bảng `user`
---
-
-INSERT INTO `permitted_file_type` (`permitted_id`, `file_type`, `max_file_size`) VALUES
-(1, 'docx', 100),
-(2, 'pdf', 100),
-(3, 'xlsx', 100),
-(4, 'png', 200);
-
-
-
-
---
--- Chỉ mục cho các bảng đã đổ
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `printer`
---
-ALTER TABLE `printer`
-  ADD PRIMARY KEY (`printer_id`);
-
---
--- Chỉ mục cho bảng `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
-
---
--- Chỉ mục cho bảng `print_request`
---
-ALTER TABLE `print_request`
-  ADD PRIMARY KEY (`request_id`);
-
---
--- Chỉ mục cho bảng `print_request`
---
-ALTER TABLE `permitted_file_type`
-  ADD PRIMARY KEY (`permitted_id`);
-
---
--- AUTO_INCREMENT cho các bảng đã đổ
---
-
---
--- AUTO_INCREMENT cho bảng `printer`
---
-ALTER TABLE `printer`
-  MODIFY `printer_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
---
--- AUTO_INCREMENT cho bảng `print_request`
---
-ALTER TABLE `print_request`
-  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
---
--- AUTO_INCREMENT cho bảng `permitted_file_type`
---
-ALTER TABLE `permitted_file_type`
-  MODIFY `permitted_id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
--- --------------------------------------------------------
-
-
-
-
-
---
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
