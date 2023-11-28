ae1856fa7b744847313b766e36192996455ffb26
Dung Vo <dung.vovandung@hcmut.edu.vn>
2023-11-28 09:50:09 +0700

    update

--- model/DAO/printer_DAO.js
+++ model/DAO/printer_DAO.js
@@ -1,4 +1,6 @@
 var connect_DB = require('./connect_db');
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcrypt");
 const getPrinters = (callback) => {
   const query = 'SELECT * FROM printer';
   connect_DB.query(query, (err, result) => {
