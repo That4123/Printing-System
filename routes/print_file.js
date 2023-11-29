bf3f5494df13410ff0fe77aed9c83729e1fae520
Nhan <hotrongnhan0687@gmail.com>
2023-11-29 22:45:33 +0700

    fix view printer

--- routes/print_file.js
+++ routes/print_file.js
@@ -3,7 +3,9 @@ const printer_request_router = express.Router();
 const path = require("path");
 const print_file_controller = require('../controllers/print_file');
 
-printer_request_router.get("/", print_file_controller.getPrinterList);
+
+printer_request_router.get("/", print_file_controller.getPrinterListID);
+printer_request_router.post("/printerDetail", print_file_controller.getPrintersDetail);
 printer_request_router.post("/makePrintRequest", print_file_controller.makePrintRequest);
 printer_request_router.post("/makeUpdateRequest", print_file_controller.makeUpdateRequest);
 printer_request_router.post("/permittedFileType", print_file_controller.handleUploadFile);
